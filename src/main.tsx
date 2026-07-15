1c4c3d47a3a42bd17607cfee34f160dd665098e3
mohd khalid <mohdkhalid3886@gmail.com>
2026-07-15 17:42:44 +0530

    updated resume

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fontsource/inter/index.css";
 
+/// comment
+
 import App from "./App";
 
 import "./styles/globals.css";
